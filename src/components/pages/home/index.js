d91ee8b476c523c1bdbaeec235b96828366097e2
fnascimento <felipegonsalves168@gmail.com>
2023-08-28 15:49:55 -0300

    28/08

--- src/components/pages/home/index.js
+++ src/components/pages/home/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import "../../../index.css"
+import logo from "../../../assets/logo192.png"
 
 export default function Home(){
 
     return(
-       <div className='App-header'>
-
+       <div>
+            <img src={logo} width={120} height={120} alt='imagem do sapato'/>
        </div>
     )
 }
